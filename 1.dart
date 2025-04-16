81b0bc65e2cdd4277e27439c65b84c115ef6dbf1
Tomas-Arsal <tomas.arsal.23@gmail.com>
2025-04-16 14:29:26 +0200

    new update

---
+++ 1.dart
@@ -0,0 +1,21 @@
+// class Car {
+//   String brand;
+//   String model;
+//   int year;
+
+//   Car(this.brand, this.model, this.year);
+
+//   void displayInfo() {
+//     print('Brand: $brand');
+//     print('Model: $model');
+//     print('Year: $year');
+//   }
+// }
+
+// void main() {
+//   Car car1 = Car('Verna', 'Model U', 2022);
+//   Car car2 = Car('Volvo', 'Corolla', 2020);
+
+//   car1.displayInfo();
+//   car2.displayInfo();
+// }
---
+++ 3.dart
@@ -0,0 +1,19 @@
+// class BankAccount {
+//   double _balance = 0;
+
+//   double get balance => _balance;
+
+//   set deposit(double amount) {
+//     if (amount < 0) {
+//       print('Error: Cannot deposit a negative amount!');
+//     } else {
+//       _balance += amount;
+//     }
+//   }
+// }
+
+// void main() {
+//   BankAccount account = BankAccount();
+//   account.deposit = 500;
+//   print('Balance: ${account.balance}');
+// }
